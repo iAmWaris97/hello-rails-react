2bb71c4b606980f763095e321f3960e6363cb24d
iAmWaris97 <haleem.waris@gmail.com>
2023-03-01 02:14:16 +0430

    added the messages to db and added route to greetings

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
+  namespace :api do
+    resources :greetings, only: :index
+  end
   # root "articles#index"
 end
